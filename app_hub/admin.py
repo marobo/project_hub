e8e4ed101ef9e7434fbf5babda7024fa0839d168
Onorio de Jesus <onorio@catalpa.io>
2025-12-10 17:22:11 +0900

    add stats page for tracking visitors

--- app_hub/admin.py
+++ app_hub/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, Contact
+from .models import Project, Contact, Visitor
 
 
 @admin.register(Project)
@@ -13,3 +13,12 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'created_at']
     search_fields = ['name', 'email']
     readonly_fields = ['name', 'email', 'message', 'created_at']
+
+
+@admin.register(Visitor)
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ['ip_address', 'page', 'visited_at']
+    list_filter = ['visited_at', 'page']
+    search_fields = ['ip_address', 'page']
+    readonly_fields = ['ip_address', 'page', 'user_agent', 'visited_at']
+    date_hierarchy = 'visited_at'
