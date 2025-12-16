29a3e503a377b34875caacab0d59a2f8ecd933c2
Onorio de Jesus <onorio@catalpa.io>
2025-12-16 17:48:05 +0900

    update media url in settings

--- project_hub/settings.py
+++ project_hub/settings.py
@@ -127,11 +127,11 @@ ADMIN_EMAIL = config('ADMIN_EMAIL')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # WhiteNoise configuration
