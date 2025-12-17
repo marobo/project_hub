e6062ec8b7062ade9d050350c0ad327a5aacfe66
Onorio de Jesus <onorio@catalpa.io>
2025-12-17 09:54:49 +0900

    Delete the image file when project is delete

--- app_hub/admin.py
+++ app_hub/admin.py
@@ -21,6 +21,21 @@ class ProjectAdmin(admin.ModelAdmin):
                 pass
         super().save_model(request, obj, form, change)
 
+    def delete_model(self, request, obj):
+        # Delete the image file when project is deleted
+        if obj.image:
+            if os.path.isfile(obj.image.path):
+                os.remove(obj.image.path)
+        super().delete_model(request, obj)
+
+    def delete_queryset(self, request, queryset):
+        # Handle bulk deletion from admin
+        for obj in queryset:
+            if obj.image:
+                if os.path.isfile(obj.image.path):
+                    os.remove(obj.image.path)
+        super().delete_queryset(request, queryset)
+
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
